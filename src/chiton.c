5a0e259074e0bc8c1b3a6c7a9d21b9b3531b3017
Jalen Adams <jalen@jalenkadams.me>
2021-06-30 21:03:15 -0400

    Add: get and tokenize user input

--- src/chiton.c
+++ src/chiton.c
@@ -9,10 +9,82 @@
  *
  */
 
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CHITON_TOK_BUFSIZE 64
+#define CHITON_TOK_DELIM " \t\r\n\a"
+
+/**
+ * @brief Reads a line from standard input.
+ *
+ * @return A pointer to the line that was read.
+ */
+char *chiton_read_line()
+{
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    if (getline(&line, &bufsize, stdin) == -1) {
+        if (feof_unlocked(stdin)) {
+            /* We've received EOF. */
+            exit(EXIT_SUCCESS);
+        }
+        else {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+/**
+ * @brief Splits a line of text into tokens.
+ *
+ * @param line The line to split.
+ * @return A list of tokens extracted from the line.
+ */
+char **chiton_split_line(char *line)
+{
+    int bufsize = CHITON_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "chiton: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, CHITON_TOK_DELIM);
+    while (token) {
+        tokens[position] = token;
+        ++position;
+
+        if (position >= bufsize) {
+            bufsize += CHITON_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "chiton: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, CHITON_TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
 
 int main(int argc, char **argv)
 {
-    printf("Hello World!\n");
+    char *line;
+
+    printf("> ");
+    line = chiton_read_line();
+
     return 0;
 }
